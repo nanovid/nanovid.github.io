4868f7df1007c1f420e284cea34385e5f5bf8222
Vidyansh Sharda <39773875+nanovid@users.noreply.github.com>
2020-06-26 12:26:46 -0400

    Delete myscripts.js

--- myscripts.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("Courses");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
